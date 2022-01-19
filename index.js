5c1b2209c793e273a2626bc1105bb9cd3a5ec415
Diego Santos Lima <dihsl1986@gmail.com>
2022-01-19 14:18:09 -0300

    teste

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var nome = document.querySelector("#exampleInputName");
 nome.value = "Diego vc vai ser o melhor programador que puder ser seja o melhor";
-nome.style.color = "green"
+nome.style.color = "green"   
+
+Diego
