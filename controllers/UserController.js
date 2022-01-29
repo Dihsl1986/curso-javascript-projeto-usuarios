355934660d714a8b05c3cb017f86d334cb9ba298
Diego Santos Lima <dihsl1986@gmail.com>
2022-01-28 23:11:32 -0300

    Validando Formulários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -92,8 +92,16 @@ class UserController{
     getValues(){
 
         let user = {};
+        let isValid = true;
         
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parenElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
         if(field.name == "gender"){
         
@@ -115,6 +123,10 @@ class UserController{
         
     
     });
+
+    if (!isValid) {
+        return false;
+    }
 
         return new User(
             user.name, 
