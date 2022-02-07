795afcdea64f0c3b6794488693c072f77240d54d
Diego Santos Lima <dihsl1986@gmail.com>
2022-02-07 18:42:29 -0300

    Usando sessionStorage 02
    continuação

--- models/User.js
+++ models/User.js
@@ -46,4 +46,17 @@ class User {
         this._photo = value;
     }
 
+    loadFromJSON(json){
+        for (let name in json){
+
+            switch(name){
+                case "_register":
+                    this[name] = new Date(json[name]);
+                break;
+                default:
+                    this[name] = json[name];
+            }
+           
+        }
+    }
 }
